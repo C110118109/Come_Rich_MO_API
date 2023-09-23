9f542ed205e718cf7f3eb71b1659091e395cf150
C110118109 <C110118109@nkust.edu.tw>
2023-09-24 00:30:58 +0800

    w2功課customer新增總筆數

--- internal/pkg/dao/gorm/gorm.go
+++ internal/pkg/dao/gorm/gorm.go
@@ -1,28 +1,68 @@
 package gorm
 
 import (
-	//"fmt"
-	//"os"
+	"fmt"
+	"os"
 
-	//"strconv"
+	"strconv"
 
-	//"eirc.app/internal/pkg/log"
+	"eirc.app/internal/pkg/log"
 	//"gorm.io/driver/postgres"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-	//"gorm.io/plugin/dbresolver"
+	"gorm.io/plugin/dbresolver"
 )
 
+// func New() (*gorm.DB, error) {
+// 	// github.com/denisenkom/go-mssqldb
+// 	dsn := "sqlserver://ManuOrder:nkuste215@163.18.42.223:1433?database=XMLY5000"
+// 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return db, nil
+// }
+
 func New() (*gorm.DB, error) {
-	// github.com/denisenkom/go-mssqldb
-	dsn := "sqlserver://ManuOrder:nkuste215@163.18.42.223:1433?database=XMLY5000"
-	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	const config string = "server=%s;user id=%s;password=%s;port=%s;database=%s"
+	sources := fmt.Sprintf(config,
+		os.Getenv("SOURCES_HOST"),
+		os.Getenv("SOURCES_USER"),
+		os.Getenv("SOURCES_PASSWORD"),
+		os.Getenv("SOURCES_PORT"),
+		os.Getenv("SOURCES_DATABASE"),
+	)
+
+	db, err := gorm.Open(sqlserver.Open(sources), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
 	if err != nil {
 		return nil, err
 	}
 
+	dbReplicas, err := strconv.Atoi(os.Getenv("DB_REPLICAS"))
+	if err != nil {
+		log.Debug(err)
+	}
+	if dbReplicas >= 1 {
+		var dialectics []gorm.Dialector
+		for i := 1; i <= dbReplicas; i++ {
+			replicas := fmt.Sprintf(config,
+				os.Getenv("REPLICAS_HOST_"+strconv.Itoa(i)),
+				os.Getenv("REPLICAS_USER_"+strconv.Itoa(i)),
+				os.Getenv("REPLICAS_PASSWORD_"+strconv.Itoa(i)),
+				os.Getenv("REPLICAS_PORT_"+strconv.Itoa(i)),
+				os.Getenv("REPLICAS_DATABASE_"+strconv.Itoa(i)),
+			)
+			connString := fmt.Sprintf("sqlserver://%s", replicas)
+			director := sqlserver.Open(connString)
+			dialectics = append(dialectics, director)
+		}
+		err = db.Use(dbresolver.Register(dbresolver.Config{Replicas: dialectics}))
+		if err != nil {
+			log.Error(err)
+		}
+	}
 	return db, nil
 }
 
